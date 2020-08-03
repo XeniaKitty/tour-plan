09caf02aba896137e9814e4e5baca7bc985c4405
XeniaKitty <rysaevak@gmail.com>
2020-08-03 20:45:06 +0300

    create: added validate for form

--- js/main.js
+++ js/main.js
@@ -50,8 +50,26 @@ $(document).ready(function() {
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
-
   };
+  //обработка формы
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalide",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Count of letters in the name must be more than 2"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Phone is required",
+        },
+      },   
+    });
+  });
 });
 
 
